b58cdfa9426bf562bfa6989cbb1bcd905b975ba2
Javiergm18 <63000031+Javiergm18@users.noreply.github.com>
2025-05-02 12:30:47 -0500

    Update usuarios.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,5 @@
-/*const express = require('express');
+/*
+const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const Usuario = require('../models/Usuario'); 
