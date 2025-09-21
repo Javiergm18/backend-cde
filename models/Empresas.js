5ef5bd5354376c54969bae759b39703c5ce95dc3
Javiergm18 <javier.guevara.2020@upb.edu.co>
2025-09-20 22:27:23 -0500

    Update Empresas.js

--- models/Empresas.js
+++ models/Empresas.js
@@ -23,26 +23,6 @@ const empresaSchema = new mongoose.Schema({
     superSociedades: String,
     fechaInicioAsesoria: Date,
     remitido: String,
-
-    fechaNacimiento: Date, // pregunta 3
-    estadoCivil: String, // pregunta 6
-    ultimoNivelAcademico: String, // pregunta 7
-    rolInstitucionEntidad: String, // pregunta 8
-    nombreEmprendimiento: String, // pregunta 10
-    anoIncioEmprendimiento: String, // pregunta 11
-    tipoEmprendimiento: String, // pregunta 12
-    productosEmprendimiento: String, // pregunta 13
-    motivoEmprendimiento: String, // pregunta 14
-    personalEmprendimiento: String, // pregunta 15
-    ubicacionEmprendimiento: String, // pregunta 16
-    nivelFormalEmprendimiento: String, // pregunta 17
-    cursosCapacitaciones: String, // pregunta 18
-    retosEmprendimiento: String, // pregunta 19
-    canalesDeVenta: String, // pregunta 20
-    apoyoEmprendimiento: String, // pregunta 21
-    disponibilidadEmprendimiento: String, // pregunta 22
-    accesibilidadEmprendimiento: String, // pregunta 23
-
     areaIntervenir: [String],
     brechaCerrar: [String],
 
