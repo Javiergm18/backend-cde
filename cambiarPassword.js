ae61c1854281faf533f1f8947529556ac27c3f88
Javiergm18 <javier.guevara.2020@upb.edu.co>
2025-10-15 12:45:20 -0500

    Update cambiarPassword.js

--- cambiarPassword.js
+++ cambiarPassword.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
-const Usuario = require('./models/usuario'); 
+const Usuario = require('./models/Usuario'); 
 
 dotenv.config(); 
 
